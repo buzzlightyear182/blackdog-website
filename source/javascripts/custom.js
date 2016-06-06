8e788684f0fffab7f7822ad9d9c6d9bb123e89a9
buzzlightyear182 <janeheatherdee@gmail.com>
2016-06-06 23:43:53 +0800

    Added news articles, learning zone article, animations, hover on map showing city

--- source/javascripts/custom.js
+++ source/javascripts/custom.js
@@ -1,7 +1,8 @@
 $(function() {
+  new WOW().init();
+
   $('.carousel-inner').ready(function(){
     $('.carousel-item:eq(1)').removeClass('active');
-    console.log("removed")
   })
 
   $('.carousel').carousel({
@@ -17,4 +18,16 @@ $(function() {
   });
 
   $('[data-toggle="tooltip"]').tooltip()
+
+  $('li.manila').hover(
+    function(){
+    cityName = $(this)[0].className;
+    iconName = '.' + cityName + '-icon'
+      $(iconName).removeClass('hidden').addClass('tada');
+    },
+    function(){
+      console.log(iconName);
+      $(iconName).removeClass('tada').addClass('hidden');
+    }
+  )
 });
